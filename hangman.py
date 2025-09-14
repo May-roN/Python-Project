421b9c1503c9538e0a7cc54c914b2cbab93d8923
May-roN <mehranali.5009@gmail.com>
2025-09-14 10:13:12 -0400

    hangman.py

---
+++ hangman.py
@@ -0,0 +1,28 @@
+import random
+
+choices = ["banana","orange","grapes","apple","watermelon","mango"]
+guessLimit = 0
+guesses = 0
+
+
+def choose_word(fruits):
+    word = random.choice(fruits)
+    print (fruits)
+    
+'''
+while guessLimit >= guesses:
+    for letter in word:
+        letterGuess = input ("Guess one letter: ")
+        print (letter)
+
+        if letterGuess == letter:
+            print (letterGuess)
+            print("correct")
+            guesses += 1
+            
+        elif letterGuess != letter:
+            print("Wrong letter, try again!")
+            guesses += 1
+
+        print ("guess counter:", guesses)
+'''
